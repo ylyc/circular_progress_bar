935faf70b91140bc1982d9f40eabea6f87a9ce14
Leon <yulangcheng@gmail.com>
2013-05-16 19:09:56 -0700

    initial checkin

---
+++ gen/com/lylc/widget/circularprogressbar/example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lylc.widget.circularprogressbar.example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
